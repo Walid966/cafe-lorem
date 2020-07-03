85000e22c1595b1080b9f36880bbb6627ab38cf0
Walid El Ahmadi <walid0896@gmail.com>
2020-07-03 13:57:37 +0100

    r

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 // connection to database
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 // $server = $url["host"];
 // $username = $url["user"];
@@ -12,7 +12,16 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 // eu-cdbr-west-03.cleardb.net
 // heroku_a5f9b39930c0939
 
-$conn = mysqli_connect("eu-cdbr-west-03.cleardb.net", "b1bebdf9e0f352", "9fc88926", "heroku_a5f9b39930c0939");
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+// $conn = new mysqli($server, $username, $password, $db);
+
+$conn = mysqli_connect($server, $username, $password, $db);
 
 // check connection
 if (!$conn) {
