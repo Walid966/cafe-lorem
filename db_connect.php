2993a4626022d2485bf5a3062357dbc9b181df99
Walid El Ahmadi <walid0896@gmail.com>
2020-07-03 02:43:56 +0100

    t

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,13 @@
 <?php
 // connection to database
-$conn = mysqli_connect("localhost", "root", "", "menu__form");
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+// $server = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $db = substr($url["path"], 1);
+
+$conn = mysqli_connect("eu-cdbr-west-03.cleardb.net", "b1bebdf9e0f352", "9fc88926", "heroku_a5f9b39930c0939");
 
 // check connection
 if (!$conn) {
